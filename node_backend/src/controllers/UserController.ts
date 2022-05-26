bacc2457ffc24f8030e7f3efd2919be3a8161194
ekankr2 <ekankr2@naver.com>
2022-05-26 19:47:34 +0900

    feat: user controller

---
+++ node_backend/src/controllers/UserController.ts
@@ -0,0 +1,118 @@
+import {
+    JsonController,
+    Get,
+    Res,
+    HttpCode,
+    Param,
+    Put,
+    Body,
+    UseBefore,
+} from "routing-controllers";
+import {Response} from "express";
+import {UserService} from "../services/UserService";
+import {OpenAPI} from "routing-controllers-openapi";
+import {UpdateUserDto} from "../dtos/UserDto";
+import {checkCookieAccessToken} from "../middlewares/AuthMiddleware";
+
+@JsonController("/users")
+export class UserController {
+    constructor(private userService: UserService) {
+    }
+
+    @HttpCode(200)
+    @Get("/:id")
+    @OpenAPI({
+        summary: "user info",
+        description: "return user info with UserId",
+        statusCode: "200",
+        responses: {
+            "400": {
+                description: "Bad request",
+            },
+        },
+    })
+    public async getOne(@Param("id") id: string, @Res() res: Response) {
+        const user = await this.userService.getUserById(id);
+
+        if (!user) {
+            return res.status(400).send({message: "No matching user."});
+        }
+
+        return user;
+    }
+
+    @HttpCode(200)
+    @Get("/:id/posts")
+    @OpenAPI({
+        summary: "get Posts of user",
+        description: "get posts written by user with UserId",
+        statusCode: "200",
+        responses: {
+            "204": {
+                description: "No Content",
+            },
+        },
+    })
+    public async getAllPosts(@Param("id") id: string, @Res() res: Response) {
+        const posts = await this.userService.getPostsByUserId(id);
+
+        if (posts.length === 0) {
+            return res.status(204).send(posts);
+        }
+
+        return posts;
+    }
+
+    @HttpCode(200)
+    @Get("/:id/comments")
+    @OpenAPI({
+        summary: "user comment list",
+        description: "return comment list of user with userId",
+        statusCode: "200",
+        responses: {
+            "204": {
+                description: "No Content",
+            },
+        },
+    })
+    public async getAllComments(@Param("id") id: string, @Res() res: Response) {
+        const comments = await this.userService.getCommentsByUserId(id);
+
+        if (comments.length === 0) {
+            return res.status(204).send(comments);
+        }
+
+        return comments;
+    }
+
+    @HttpCode(200)
+    @Put("/:id")
+    @OpenAPI({
+        summary: "update user info",
+        statusCode: "200",
+        responses: {
+            "403": {
+                description: "Forbidden",
+            },
+        },
+    })
+    @UseBefore(checkCookieAccessToken)
+    public async update(
+        @Param("id") id: string,
+        @Body() updateUserDto: UpdateUserDto,
+        @Res() res: Response,
+    ) {
+        const {userId} = res.locals.jwtPayload;
+        const updatedUser = await this.userService.updateUser(
+            userId,
+            id,
+            updateUserDto,
+        );
+
+        if (!updatedUser) {
+            return res.status(403).send({message: "You have no permission"});
+        }
+
+        return updatedUser;
+    }
+}
--- node_backend/src/entities/User.ts
+++ node_backend/src/entities/User.ts
@@ -1,51 +1,51 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
-  BeforeInsert,
-  OneToMany,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    BeforeInsert,
+    OneToMany,
 } from "typeorm";
-import { IsNotEmpty } from "class-validator";
+import {IsNotEmpty} from "class-validator";
 import bcrypt from "bcrypt";
-import { Post } from "./Post";
+import {Post} from "./Post";
 
-@Entity({ name: "user" })
+@Entity({name: "user"})
 export class User {
-  @PrimaryGeneratedColumn("uuid")
-  id: string;
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
 
-  @IsNotEmpty()
-  @Column({ name: "real_name" })
-  realName: string;
+    @IsNotEmpty()
+    @Column({name: "real_name", length: 50})
+    realName: string;
 
-  @IsNotEmpty()
-  @Column({ precision: 100 })
-  email: string;
+    @IsNotEmpty()
+    @Column({length: 100})
+    email: string;
 
-  @IsNotEmpty()
-  @Column({ select: false })
-  password: string;
+    @IsNotEmpty()
+    @Column({select: false})
+    password: string;
 
-  @Column({ name: "refresh_token", nullable: true, select: false })
-  refreshToken: string;
+    @Column({name: "refresh_token", nullable: true, select: false})
+    refreshToekn: string;
 
-  @CreateDateColumn({ name: "created_at" })
-  createdAt: Date;
+    @CreateDateColumn({name: "created_at"})
+    createdAt: Date;
 
-  @UpdateDateColumn({ name: "updated_at" })
-  updatedAt: Date;
+    @UpdateDateColumn({name: "updated_at"})
+    updatedAt: Date;
 
-  @OneToMany((type) => Post, (post) => post.user)
-  posts: Post[];
+    @OneToMany((type) => Post, (post) => post.user)
+    posts: Post[];
 
-  @BeforeInsert()
-  async hashPassword() {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
 
-  async comparePassword(unencryptedPassword: string): Promise<boolean> {
-    return await bcrypt.compare(unencryptedPassword, this.password);
-  }
+    async comparePassword(unencryptedPassword: string): Promise<boolean> {
+        return await bcrypt.compare(unencryptedPassword, this.password);
+    }
 }
