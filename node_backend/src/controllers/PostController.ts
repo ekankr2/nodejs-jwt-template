48bc081a92fa449d2822e5f306747cc907f11de3
ekankr2 <ekankr2@naver.com>
2022-05-27 18:19:13 +0900

    feat: create bulk post controller

--- node_backend/src/controllers/PostController.ts
+++ node_backend/src/controllers/PostController.ts
@@ -1,154 +1,173 @@
-import { PostService } from "../services/PostService";
+import {PostService} from "../services/PostService";
 import {
-  JsonController,
-  Get,
-  Param,
-  Body,
-  Post,
-  Put,
-  UseBefore,
-  Res,
-  Delete,
-  HttpCode,
-  QueryParams,
+    JsonController,
+    Get,
+    Param,
+    Body,
+    Post,
+    Put,
+    UseBefore,
+    Res,
+    Delete,
+    HttpCode,
+    QueryParams,
 } from "routing-controllers";
 import {checkCookieAccessToken} from "../middlewares/AuthMiddleware";
-import { Response } from "express";
-import { OpenAPI, ResponseSchema } from "routing-controllers-openapi";
-import {CreatePostDto, UpdatePostDto, PageablePostDto} from "../dtos/PostDto";
+import {Response} from "express";
+import {OpenAPI, ResponseSchema} from "routing-controllers-openapi";
+import {CreatePostDto, UpdatePostDto, PageablePostDto, CreateBulkPostDto} from "../dtos/PostDto";
 
 @JsonController("/posts")
 export class PostController {
-  constructor(
-      private postService: PostService,
-      ) {}
-
-  @HttpCode(201)
-  @Post()
-  @OpenAPI({
-    summary: "create Post",
-    statusCode: "201",
-  })
-  @UseBefore(checkCookieAccessToken)
-  public async create(
-    @Body() createPostDto: CreatePostDto,
-    @Res() res: Response,
-  ) {
-    const { userId } = res.locals.jwtPayload;
-    return await this.postService.createPost(createPostDto, userId);
-  }
-
-  @HttpCode(200)
-  @Get("")
-  @OpenAPI({
-    summary: "get Post list",
-    statusCode: "200",
-    responses: {
-      "204": {
-        description: "No Content",
-      },
-    },
-  })
-  @ResponseSchema(PageablePostDto)
-  public async getAll(
-    @QueryParams() pageablePostDto: PageablePostDto,
-    @Res() res: Response,
-  ) {
-    const posts = await this.postService.getPosts(
-      pageablePostDto.offset,
-      pageablePostDto.limit,
-      pageablePostDto.sort,
-    );
-
-    if (posts.length === 0) {
-      return res.status(204).send(posts);
+    constructor(
+        private postService: PostService,
+    ) {
     }
 
-    return posts;
-  }
-
-  @HttpCode(200)
-  @Get("/:id")
-  @OpenAPI({
-    summary: "get Post",
-    statusCode: "200",
-    responses: {
-      "400": {
-        description: "Bad request",
-      },
-    },
-  })
-  public async getOne(@Param("id") id: string, @Res() res: Response) {
-    const post = await this.postService.getPostById(id);
-
-    if (post) {
-      await this.postService.incrementPostView(post);
-    } else {
-      return res.status(400).send({ message: "no matching post." });
+    @HttpCode(201)
+    @Post()
+    @OpenAPI({
+        summary: "create Post",
+        statusCode: "201",
+    })
+    @UseBefore(checkCookieAccessToken)
+    public async create(
+        @Body() createPostDto: CreatePostDto,
+        @Res() res: Response,
+    ) {
+        const {userId} = res.locals.jwtPayload;
+        return await this.postService.createPost(createPostDto, userId);
     }
 
-    return post;
-  }
-
-  @HttpCode(200)
-  @Put("/:id")
-  @OpenAPI({
-    summary: "update Post",
-    statusCode: "200",
-    responses: {
-      "403": {
-        description: "Forbidden",
-      },
-    },
-  })
-  @UseBefore(checkCookieAccessToken)
-  public async update(
-    @Param("id") id: string,
-    @Body() updatePostDto: UpdatePostDto,
-    @Res() res: Response,
-  ) {
-    const { userId } = res.locals.jwtPayload;
-    const updatedPost = await this.postService.updatePost(
-      id,
-      updatePostDto,
-      userId,
-    );
-
-    if (!updatedPost) {
-      return res
-        .status(403)
-        .send({ message: "You have no permission to update post." });
+    @HttpCode(201)
+    @Post()
+    @OpenAPI({
+        summary: "create bulk Post",
+        description: "create many Posts in a row",
+        statusCode: "201"
+    })
+    @UseBefore(checkCookieAccessToken)
+    public async createBulk(
+        @Body() createBulkPostDto: CreateBulkPostDto,
+        @Res() res: Response
+    ) {
+        const {userId} = res.locals.jwtPayload;
+        const newPost = await this.postService.createBulkPost(createBulkPostDto, userId)
     }
 
-    return updatedPost;
-  }
+    @HttpCode(200)
+    @Get("")
+    @OpenAPI({
+        summary: "get Post list",
+        statusCode: "200",
+        responses: {
+            "204": {
+                description: "No Content",
+            },
+        },
+    })
+    @ResponseSchema(PageablePostDto)
+    public async getAll(
+        @QueryParams() pageablePostDto: PageablePostDto,
+        @Res() res: Response,
+    ) {
+        const posts = await this.postService.getPosts(
+            pageablePostDto.offset,
+            pageablePostDto.limit,
+            pageablePostDto.sort,
+        );
 
-  @HttpCode(200)
-  @Delete("/:id")
-  @OpenAPI({
-    summary: "delete Post",
-    statusCode: "200",
-    responses: {
-      "403": {
-        description: "Forbidden",
-      },
-    },
-  })
-  @UseBefore(checkCookieAccessToken)
-  public async delete(@Param("id") id: string, @Res() res: Response) {
-    const { userId } = res.locals.jwtPayload;
+        if (posts.length === 0) {
+            return res.status(204).send(posts);
+        }
 
-    const result = await this.postService.deletePost(id, userId);
-
-    if (!result) {
-      return res
-        .status(403)
-        .send({ message: "You have no permission to delete post." });
+        return posts;
     }
 
-    return {
-      postId: id,
-      isDelete: result,
-    };
-  }
+    @HttpCode(200)
+    @Get("/:id")
+    @OpenAPI({
+        summary: "get Post",
+        statusCode: "200",
+        responses: {
+            "400": {
+                description: "Bad request",
+            },
+        },
+    })
+    public async getOne(@Param("id") id: string, @Res() res: Response) {
+        const post = await this.postService.getPostById(id);
+
+        if (post) {
+            await this.postService.incrementPostView(post);
+        } else {
+            return res.status(400).send({message: "no matching post."});
+        }
+
+        return post;
+    }
+
+    @HttpCode(200)
+    @Put("/:id")
+    @OpenAPI({
+        summary: "update Post",
+        statusCode: "200",
+        responses: {
+            "403": {
+                description: "Forbidden",
+            },
+        },
+    })
+    @UseBefore(checkCookieAccessToken)
+    public async update(
+        @Param("id") id: string,
+        @Body() updatePostDto: UpdatePostDto,
+        @Res() res: Response,
+    ) {
+        const {userId} = res.locals.jwtPayload;
+        const updatedPost = await this.postService.updatePost(
+            id,
+            updatePostDto,
+            userId,
+        );
+
+        if (!updatedPost) {
+            return res
+                .status(403)
+                .send({message: "You have no permission to update post."});
+        }
+
+        return updatedPost;
+    }
+
+    @HttpCode(200)
+    @Delete("/:id")
+    @OpenAPI({
+        summary: "delete Post",
+        statusCode: "200",
+        responses: {
+            "403": {
+                description: "Forbidden",
+            },
+        },
+    })
+    @UseBefore(checkCookieAccessToken)
+    public async delete(@Param("id") id: string, @Res() res: Response) {
+        const {userId} = res.locals.jwtPayload;
+
+        const result = await this.postService.deletePost(id, userId);
+
+        if (!result) {
+            return res
+                .status(403)
+                .send({message: "You have no permission to delete post."});
+        }
+
+        return {
+            postId: id,
+            isDelete: result,
+        };
+    }
+
+
 }
